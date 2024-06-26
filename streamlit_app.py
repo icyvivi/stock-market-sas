05bf026dfd19e9ff07d73f13271ee32006665b08
icyvivi <51418459+icyvivi@users.noreply.github.com>
2024-06-26 15:02:50 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,239 +23,125 @@ import time  # time library
 from streamlit_option_menu import option_menu  # select_options library
 
 # Page title
-st.set_page_config(page_title='ML model builder', page_icon='🏗️',)
+st.set_page_config(page_title='ML model builder', layout="wide", #initial_sidebar_state="expanded", 
+page_icon='🏗️')
 st.title('🏗️ ML model builder')
 
-#st.markdown('**Select company**')
-ticker = st.select_slider('**Select ticker**', options=['D05.SI', 'NVDA', 'NIO'])
+st.sidebar.write('''# SAS ''')
+with st.sidebar: 
+        sidebar_menu_list = ["Stocks Performance Comparison", "Real-Time Stock Price", "Stock Prediction", 'About']
+        selected = option_menu("Data Analytics", sidebar_menu_list)
 
-with st.expander('About this app'):
-  st.markdown('**What can this app do?**')
-  st.info('This app allow users to get stock signals')
+# read csv file
+stock_df = pd.read_csv("tickers_list.csv")
+#dict_csv = pd.read_csv('tickers_list.csv', header=None, index_col=0).to_dict()[1]  # read csv file
+dict_csv = pd.read_csv('sgx_all_stocks_merged.csv')[['trading_name', 'ticker']].sort_values(['trading_name']).set_index('trading_name').to_dict()['ticker']
 
-  st.markdown('**How to use the app?**')
-  st.warning('1. Select a stock name and 2. See the prediction.')
 
-  st.markdown('**Under the hood**')
-  st.markdown('Data sets:')
-  st.code('''- OHLCV from Yahoo Finance
-  ''', language='markdown')
-  
-  st.markdown('Some Libraries used:')
-  st.code('''- Pandas for data wrangling
-- Scikit-learn for building a machine learning model
-- Plotly for chart creation
-- Streamlit for user interface
-  ''', language='markdown')
+# Stock Performance Comparison Section Starts Here
+if(selected == sidebar_menu_list[0]):  # if user selects 'Stocks Performance Comparison'
+    st.subheader("Stocks Performance Comparison")
+    #stock_df["Company Name"]
+    tickers = stock_df["trading_name"].sort_values()
+    # tickers = stock_df["ticker"]
+    #tickers = ['D05.SI', 'NVDA', 'NIO']
+    # dropdown for selecting assets
+    dropdown = st.multiselect('Pick your assets', tickers)
 
-import yfinance as yf
-# Sidebar for accepting input parameters
-with st.sidebar:
-    # Load data
-    st.header('1.1. Input data')
+    with st.spinner('Loading...'):  # spinner while loading
+        time.sleep(2)
 
-    # Select example data
-    #st.markdown('**Select company**')
-    #ticker = st.select_slider('Select Yahoo ticker', options=['D05.SI', 'NVDA', 'NIO'])
-    
-    #df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv')
-    #df = pd.read_csv('/workspaces/stock-market-sas/D05.SI.csv')
-    #ticker = 'D05.SI'
-    df = yf.Ticker(ticker).history(period='max', interval='1d', auto_adjust=False, back_adjust=False).reset_index()
-    df['ticker'] = ticker
-    df['datetime'] = pd.to_datetime(df['Date'])
-    df['date'] = df['datetime'].dt.date
-    #df['time'] = df['datetime'].dt.time
-    df['year'] = df['datetime'].dt.year
-    df['month'] = df['datetime'].dt.month
-    df['day'] = df['datetime'].dt.day
-    df['prediction_target'] = df.groupby(['ticker'], as_index=False)['Adj Close'].pct_change(22).shift(-22)
-    df.dropna(inplace=True)
-    df.drop(columns=['Date', 'ticker', 'datetime', 'date'], inplace=True)
+    symb_list = []  # list for storing symbols
+    for i in dropdown:  # for each asset selected
+        ticker = dict_csv.get(i)  # get symbol from csv file
+        symb_list.append(ticker)  # append symbol to list
+    ticker = symb_list[0]  # forcing to 1 ticker first
 
-    st.header('2. Set Parameters')
-    parameter_split_size = st.slider('Data split ratio (% for Training Set)', 10, 90, 80, 1)
 
-    st.subheader('2.1. Learning Parameters')
-    with st.expander('See parameters'):
-        parameter_n_estimators = st.slider('Number of estimators (n_estimators)', 10, 1000, 100, 100)
-        parameter_max_features = st.select_slider('Max features (max_features)', options=['all', 'sqrt', 'log2'])
-        parameter_min_samples_split = st.slider('Minimum number of samples required to split an internal node (min_samples_split)', 2, 10, 2, 1)
-        parameter_min_samples_leaf = st.slider('Minimum number of samples required to be at a leaf node (min_samples_leaf)', 1, 10, 2, 1)
+    def download_ticker(ticker):
+        df = yf.Ticker(ticker).history(period='max', interval='1d', auto_adjust=False, back_adjust=False).reset_index()
+        df['ticker'] = ticker
+        df['datetime'] = pd.to_datetime(df['Date'])
+        df['date'] = df['datetime'].dt.date
+        #df['time'] = df['datetime'].dt.time
+        df['year'] = df['datetime'].dt.year
+        df['month'] = df['datetime'].dt.month
+        df['day'] = df['datetime'].dt.day
+        df['prediction_target'] = df.groupby(['ticker'], as_index=False)['Adj Close'].pct_change(22).shift(-22)
+        df.dropna(inplace=True)
+        df.drop(columns=['Date', 'ticker', 'datetime', 'date'], inplace=True)
+        return df
 
-    st.subheader('2.2. General Parameters')
-    with st.expander('See parameters', expanded=False):
-        parameter_random_state = st.slider('Seed number (random_state)', 0, 1000, 42, 1)
-        parameter_criterion = st.select_slider('Performance measure (criterion)', options=['squared_error', 'absolute_error', 'friedman_mse'])
-        parameter_bootstrap = st.select_slider('Bootstrap samples when building trees (bootstrap)', options=[True, False])
-        parameter_oob_score = st.select_slider('Whether to use out-of-bag samples to estimate the R^2 on unseen data (oob_score)', options=[False, True])
+    def relativeret(df):  # function for calculating relative return
+        rel = df.pct_change()  # calculate relative return
+        cumret = (1+rel).cumprod() - 1  # calculate cumulative return
+        cumret = cumret.fillna(0)  # fill NaN values with 0
+        return cumret  # return cumulative return
 
-    sleep_time = st.slider('Sleep time', 0, 3, 0)
+    if len(dropdown) > 0:  # if user selects atleast one asset
+        #df = relativeret(yf.download(symb_list, start, end))['Adj Close']  # download data from yfinance
+        df = download_ticker(ticker)
+        df = relativeret(df)['Adj Close']  # download data from yfinance
+        # download data from yfinance
+        raw_df = relativeret(yf.download(symb_list, start, end))
+        raw_df.reset_index(inplace=True)  # reset index
 
-# Initiate the model building process
-if df is not None: 
-    with st.status("Running ...", expanded=True) as status:
-    
-        st.write("Loading data ...")
-        time.sleep(sleep_time)
-
-        st.write("Preparing data ...")
-        time.sleep(sleep_time)
-        X = df.iloc[:,:-1]
-        y = df.iloc[:,-1]
-            
-        st.write("Splitting data ...")
-        time.sleep(sleep_time)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=(parameter_split_size)/100, test_size=(100-parameter_split_size)/100, random_state=parameter_random_state)
-    
-        st.write("Model training ...")
-        time.sleep(sleep_time)
-
-        if parameter_max_features == 'all':
-            parameter_max_features = None
-            parameter_max_features_metric = X.shape[1]
+        closingPrice = yf.download(symb_list, start, end)[
+            'Adj Close']  # download data from yfinance
+        volume = yf.download(symb_list, start, end)['Volume']
         
-        rf = RandomForestRegressor(
-                n_estimators=parameter_n_estimators,
-                max_features=parameter_max_features,
-                min_samples_split=parameter_min_samples_split,
-                min_samples_leaf=parameter_min_samples_leaf,
-                random_state=parameter_random_state,
-                criterion=parameter_criterion,
-                bootstrap=parameter_bootstrap,
-                oob_score=parameter_oob_score)
-        rf.fit(X_train, y_train)
-        
-        st.write("Applying model to make predictions ...")
-        time.sleep(sleep_time)
-        y_train_pred = rf.predict(X_train)
-        y_test_pred = rf.predict(X_test)
-            
-        st.write("Evaluating performance metrics ...")
-        time.sleep(sleep_time)
-        train_mse = mean_squared_error(y_train, y_train_pred)
-        train_r2 = r2_score(y_train, y_train_pred)
-        test_mse = mean_squared_error(y_test, y_test_pred)
-        test_r2 = r2_score(y_test, y_test_pred)
-        
-        st.write("Displaying performance metrics ...")
-        time.sleep(sleep_time)
-        parameter_criterion_string = ' '.join([x.capitalize() for x in parameter_criterion.split('_')])
-        #if 'Mse' in parameter_criterion_string:
-        #    parameter_criterion_string = parameter_criterion_string.replace('Mse', 'MSE')
-        rf_results = pd.DataFrame(['Random forest', train_mse, train_r2, test_mse, test_r2]).transpose()
-        rf_results.columns = ['Method', f'Training {parameter_criterion_string}', 'Training R2', f'Test {parameter_criterion_string}', 'Test R2']
-        # Convert objects to numerics
-        for col in rf_results.columns:
-            rf_results[col] = pd.to_numeric(rf_results[col], errors='ignore')
-        # Round to 3 digits
-        rf_results = rf_results.round(3)
-        
-    status.update(label="Status", state="complete", expanded=False)
+        st.subheader('Raw Data {}'.format(dropdown))
+        st.write(raw_df)  # display raw data
+        chart = ('Line Chart', 'Area Chart', 'Bar Chart')  # chart types
+        # dropdown for selecting chart type
+        dropdown1 = st.selectbox('Pick your chart', chart)
+        with st.spinner('Loading...'):  # spinner while loading
+            time.sleep(2)
 
-    # Display data info
-    st.header('Input data', divider='rainbow')
-    #col = st.columns(4)
-    #col[0].metric(label="No. of samples", value=X.shape[0], delta="")
-    #col[1].metric(label="No. of X variables", value=X.shape[1], delta="")
-    #col[2].metric(label="No. of Training samples", value=X_train.shape[0], delta="")
-    #col[3].metric(label="No. of Test samples", value=X_test.shape[0], delta="")
-    
-    with st.expander(f'Initial dataset : {X.shape[0]} samples with {X.shape[1]} variables', expanded=False):
-        st.dataframe(df, height=210, use_container_width=True)
-    with st.expander(f'Train set : {X_train.shape[0]} samples', expanded=False):
-        train_col = st.columns((3,1))
-        with train_col[0]:
-            st.markdown('**X_train**')
-            st.dataframe(X_train, height=210, hide_index=True, use_container_width=True)
-        with train_col[1]:
-            st.markdown('**y_train**')
-            st.dataframe(y_train, height=210, hide_index=True, use_container_width=True)
-    with st.expander(f'Test set : {X_test.shape[0]} samples', expanded=False):
-        test_col = st.columns((3,1))
-        with test_col[0]:
-            st.markdown('**X_test**')
-            st.dataframe(X_test, height=210, hide_index=True, use_container_width=True)
-        with test_col[1]:
-            st.markdown('**y_test**')
-            st.dataframe(y_test, height=210, hide_index=True, use_container_width=True)
+        st.subheader('Relative Returns {}'.format(dropdown))
+                
+        if (dropdown1) == 'Line Chart':  # if user selects 'Line Chart'
+            st.line_chart(df)  # display line chart
+            # display closing price of selected assets
+            st.write("### Closing Price of {}".format(dropdown))
+            st.line_chart(closingPrice)  # display line chart
 
-    # Zip dataset files
-    df.to_csv('dataset.csv', index=False)
-    X_train.to_csv('X_train.csv', index=False)
-    y_train.to_csv('y_train.csv', index=False)
-    X_test.to_csv('X_test.csv', index=False)
-    y_test.to_csv('y_test.csv', index=False)
-    
-    list_files = ['dataset.csv', 'X_train.csv', 'y_train.csv', 'X_test.csv', 'y_test.csv']
-    with zipfile.ZipFile('dataset.zip', 'w') as zipF:
-        for file in list_files:
-            zipF.write(file, compress_type=zipfile.ZIP_DEFLATED)
+            # display volume of selected assets
+            st.write("### Volume of {}".format(dropdown))
+            st.line_chart(volume)  # display line chart
 
-    with open('dataset.zip', 'rb') as datazip:
-        btn = st.download_button(
-                label='Download ZIP',
-                data=datazip,
-                file_name="dataset.zip",
-                mime="application/octet-stream"
-                )
-    
-    # Display model parameters
-    st.header('Model parameters', divider='rainbow')
-    parameters_col = st.columns(3)
-    parameters_col[0].metric(label="Data split ratio (% for Training Set)", value=parameter_split_size, delta="")
-    parameters_col[1].metric(label="Number of estimators (n_estimators)", value=parameter_n_estimators, delta="")
-    parameters_col[2].metric(label="Max features (max_features)", value=parameter_max_features_metric, delta="")
-    
-    # Display feature importance plot
-    importances = rf.feature_importances_
-    feature_names = list(X.columns)
-    forest_importances = pd.Series(importances, index=feature_names)
-    df_importance = forest_importances.reset_index().rename(columns={'index': 'feature', 0: 'value'})
-    
-    bars = alt.Chart(df_importance).mark_bar(size=40).encode(
-             x='value:Q',
-             y=alt.Y('feature:N', sort='-x')
-           ).properties(height=250)
+        elif (dropdown1) == 'Area Chart':  # if user selects 'Area Chart'
+            st.area_chart(df)  # display area chart
+            # display closing price of selected assets
+            st.write("### Closing Price of {}".format(dropdown))
+            st.area_chart(closingPrice)  # display area chart
 
-    performance_col = st.columns((2, 0.2, 3))
-    with performance_col[0]:
-        st.header('Model performance', divider='rainbow')
-        st.dataframe(rf_results.T.reset_index().rename(columns={'index': 'Parameter', 0: 'Value'}))
-    with performance_col[2]:
-        st.header('Feature importance', divider='rainbow')
-        st.altair_chart(bars, theme='streamlit', use_container_width=True)
+            # display volume of selected assets
+            st.write("### Volume of {}".format(dropdown))
+            st.area_chart(volume)  # display area chart
 
-    # Prediction results
-    st.header('Prediction results', divider='rainbow')
-    s_y_train = pd.Series(y_train, name='actual').reset_index(drop=True)
-    s_y_train_pred = pd.Series(y_train_pred, name='predicted').reset_index(drop=True)
-    df_train = pd.DataFrame(data=[s_y_train, s_y_train_pred], index=None).T
-    df_train['class'] = 'train'
-        
-    s_y_test = pd.Series(y_test, name='actual').reset_index(drop=True)
-    s_y_test_pred = pd.Series(y_test_pred, name='predicted').reset_index(drop=True)
-    df_test = pd.DataFrame(data=[s_y_test, s_y_test_pred], index=None).T
-    df_test['class'] = 'test'
-    
-    df_prediction = pd.concat([df_train, df_test], axis=0)
-    
-    prediction_col = st.columns((2, 0.2, 3))
-    
-    # Display dataframe
-    with prediction_col[0]:
-        st.dataframe(df_prediction, height=320, use_container_width=True)
+        elif (dropdown1) == 'Bar Chart':  # if user selects 'Bar Chart'
+            st.bar_chart(df)  # display bar chart
+            # display closing price of selected assets
+            st.write("### Closing Price of {}".format(dropdown))
+            st.bar_chart(closingPrice)  # display bar chart
 
-    # Display scatter plot of actual vs predicted values
-    with prediction_col[2]:
-        scatter = alt.Chart(df_prediction).mark_circle(size=60).encode(
-                        x='actual',
-                        y='predicted',
-                        color='class'
-                  )
-        st.altair_chart(scatter, theme='streamlit', use_container_width=True)
+            # display volume of selected assets
+            st.write("### Volume of {}".format(dropdown))
+            st.bar_chart(volume)  # display bar chart
 
-    
-# Ask for Company
-else:
-    st.warning('👈 Select a company to get started!')
+        else:
+            st.line_chart(df, width=1000, height=800,
+                          use_container_width=False)  # display line chart
+            # display closing price of selected assets
+            st.write("### Closing Price of {}".format(dropdown))
+            st.line_chart(closingPrice)  # display line chart
+
+            # display volume of selected assets
+            st.write("### Volume of {}".format(dropdown))
+            st.line_chart(volume)  # display line chart
+
+    else:  # if user doesn't select any asset
+        #dropdown = ['NVDA']
+        st.write('Please select at least one asset')  # display message
+# Stock Performance Comparison Section Ends Here
