1d423a83736a670a18fbee8d25329667d8f4f857
icyvivi <51418459+icyvivi@users.noreply.github.com>
2024-06-29 03:47:08 +0000

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,9 +64,10 @@ if(selected == sidebar_menu_list[0]):  # if user selects 'Stocks Performance Com
         df['datetime'] = pd.to_datetime(df['Date'])
         df['date_local'] = df['datetime'].dt.date
         # df['time'] = df['datetime'].dt.time
-        df['year'] = df['datetime'].dt.year
+        df['year'] = df['datetime'].dt.year.astype(int)
         df['month'] = df['datetime'].dt.month
         df['day'] = df['datetime'].dt.day
+        df['transaction_estimate'] = df['Adj Close'] * df['Volume']
         # df['prediction_target'] = df.groupby(['ticker'], as_index=False)['Adj Close'].pct_change(22).shift(-22)
         # df.dropna(inplace=True)
         #df.drop(columns=['Date', 
@@ -79,23 +80,60 @@ if(selected == sidebar_menu_list[0]):  # if user selects 'Stocks Performance Com
         start = datetime.date(1900, 1, 1)
         end = datetime.date.today()
         df = download_ticker(ticker)
-        closingPrice = df['Adj Close']
-        volume = df['Volume']
+        #closingPrice = df['Adj Close']
+        #volume = df['Volume']
 
         st.subheader('OHLCV')
         st.dataframe(df.sort_values(['Date'], ascending=False).reset_index(drop=True), height=210, hide_index=False, use_container_width=True)
         # st.write(df)  # display raw data
-        chart = ('Line Chart', 'Area Chart', 'Bar Chart')  # chart types
+        #chart = ('Line Chart', 'Area Chart', 'Bar Chart')  # chart types
         # dropdown for selecting chart type
         #dropdown1 = st.selectbox('Pick your chart', chart)
         #with st.spinner('Loading...'):  # spinner while loading
         #    time.sleep(2)
 
         #st.subheader('{}'.format(ticker))
-        plotly_fig = px.line(df, x='Date', y='Adj Close', 
-                             template='plotly_dark',
-                             title=ticker)
-        st.write(plotly_fig)
+        #plotly_fig = px.line(df, x='Date', y='Adj Close', 
+        #                     template='plotly_dark',
+        #                     title=ticker)
+        fig_date = df['date_local'] #df['Date']
+        fig_open = df['Open']
+        fig_high = df['High']
+        fig_low = df['Low']
+        fig_close = df['Close']
+        fig_price = df['Adj Close']
+        fig_vol = df['Volume']
+
+        fig = make_subplots(rows=2, cols=1, shared_xaxes=True, row_heights=[0.7, 0.3])
+        fig.add_trace(go.Candlestick(x=fig_date,
+                             open=fig_open, high=fig_high, low=fig_low, close=fig_close,
+                             increasing_line_color='green', decreasing_line_color='red',
+                             name='OHLC'),
+              row=1, col=1)
+        fig.add_trace(go.Bar(x=fig_date, y=fig_vol,
+                     marker=dict(color=np.where(fig_close.diff() >= 0, 'green', 'red')),
+                     name='Volume'),
+              row=2, col=1)
+
+
+        # Update layout for better readability
+        fig.update_layout(title = ticker,
+                          #title=f"{str(df['Company'].unique()[0])} triggers since {str(model_last_date.date())} (Score : Buy {df_backtest[df_backtest['trade_action'] == 'buy']['f1-score'].iloc[0]:.0%} , Sell {df_backtest[df_backtest['trade_action'] == 'sell']['f1-score'].iloc[0]:.0%})",
+                        #xaxis_title="Date",
+                        yaxis_title="Price",
+                        yaxis2_title="Volume",
+                        legend_title="Legends",
+                        plot_bgcolor='black',
+                        paper_bgcolor='black',
+                        font=dict(color='white'),
+                        showlegend=True,
+                        xaxis_rangeslider_visible=False)
+
+        # Remove gridlines
+        fig.update_xaxes(showgrid=False, type='category')
+        fig.update_yaxes(showgrid=False, row=1, col=1)
+        fig.update_yaxes(showgrid=False, row=2, col=1)
+        st.write(fig)
         #st.line_chart(df, x='Date', y='Adj Close', # width=1000, height=800, 
         #              use_container_width=False)  # display line chart
         #st.bar_chart(df, x='Date', y='Volume')  # display bar chart
