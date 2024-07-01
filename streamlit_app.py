880672d6cd05a1daefea22af60b34a35517b4e53
icyvivi <51418459+icyvivi@users.noreply.github.com>
2024-07-01 16:28:06 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -132,8 +132,8 @@ if(selected == sidebar_menu_list[0]):  # if user selects 'Stocks TA'
 
         #fig.update_layout(title="S&P500 vs. VIX")
         fig.update_xaxes(title_text="Date")
-        fig.update_yaxes(title_text="S&P500 Adj. Close", secondary_y=False)
-        fig.update_yaxes(title_text="VIX Adj. Close", secondary_y=True)
+        fig.update_yaxes(title_text="S&P500", secondary_y=False)
+        fig.update_yaxes(title_text="VIX", secondary_y=True)
 
         # Update layout for better readability
         fig.update_layout(#title = f'S&P 500 [{ticker}]',
